a5bf38793f5b04a7349d22e2267ced59b4569847
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-05-25 16:47:55 -0500

    basic directory and markup structure in place

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import Calculator from './Calculator.js'
+
 import '../css/App.css'
 
 const App = () => {
     return (
-        <div className='app'></div>
+        <div className='app'>
+            <Calculator/>
+        </div>
     )
 }
 
