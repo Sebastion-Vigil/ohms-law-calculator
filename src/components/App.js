dee630f48542507a3ed37894ed15891648deace8
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-05-28 17:21:50 -0500

    ExampleClassComponentWithMultipleStateProperties.js is instantly useful

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// we're going to do this the 'right way' this time
+// https://www.digitalocean.com/community/tutorials/five-ways-to-convert-react-class-components-to-functional-components-with-react-hooks
+
 import Calculator from './Calculator.js'
 
 import '../css/App.css'
