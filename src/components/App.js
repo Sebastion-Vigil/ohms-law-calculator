ac354b31df3dce51a04833274c4ea9bb30cca9db
link <irishseanvaldivia@gmail.com>
2021-06-02 17:13:19 -0500

    another link to learny learny React Hooks will b worth it in the end

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 // we're going to do this the 'right way' this time
+// https://reactjs.org/docs/hooks-overview.html
 // https://www.digitalocean.com/community/tutorials/five-ways-to-convert-react-class-components-to-functional-components-with-react-hooks
 
 import Calculator from './Calculator.js'
