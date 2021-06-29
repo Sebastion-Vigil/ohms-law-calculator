86b3ea41ab176fd86f4f3209dea0e8e82b87a8a1
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-06-29 13:31:43 -0500

    user selected a value! Watts

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,7 +6,7 @@ import '../css/calculator.css'
 // learny learny https://stackoverflow.com/questions/54620928/useeffect-hook-not-firing-after-state-change#54621059
 const Calculator = () => {
   const titles = useState([
-    // 3 main states of app process:
+    // 3 main steps of app process:
     'Searching for which value?', // 1 Select value
     'Which 2 values known?', // 2 Get two known values
     'Answer' // 3 Calculate and return answer
@@ -14,13 +14,14 @@ const Calculator = () => {
   const values = useState(['Watts', 'Volts', 'Ohms', 'Amps'])
   const [renderedTitle, setRenderedTitle] = useState(0) // index into titles, updates as needed
   const [renderedValues, setRenderedVals] = useState([0, 1, 2, 3]) // indexes into values, splice as needed
+  const [userInputVals, setUserInputVals] = useState([undefined, undefined]) // both truthy when value pair complete
 
   useEffect(() => {
     console.log('Calculator useEffect invoked')
-  }, [renderedValues]) // fires every time dependency changes
+  }, [renderedValues, userInputVals]) // fires onMount & every time dependency changes
 
   const userSelectValue = val => {
-    if (renderedTitle > 0) return
+    // console.log('userInputVals in userSelectValue: ', userInputVals[0], userInputVals[1])
     console.log('user selected a value!', values[0][val])
     const updatedVals = [...renderedValues]
     updatedVals.splice(val, 1)
@@ -29,22 +30,35 @@ const Calculator = () => {
   }
 
   const userInputValue = val => {
-    console.log('user input value!', val)
+    // console.log('userInputVals in userInputValue: ', userInputVals[0], userInputVals[1])
+    const newTitle = userInputVals[0] && !userInputVals[1] ? 2 : renderedTitle
+    const updatedUserInput = [...userInputVals]
+    // console.log('user input value!', val)
+    if (!userInputVals[0] && !userInputVals[1]) {
+      console.log('Recording first user input', values[0][val])
+      updatedUserInput[0] = values[0][val]
+    } 
+    if (userInputVals[0] && !userInputVals[1]) {
+      console.log('Recording second user input', values[0][val])
+      updatedUserInput[1] = values[0][val]
+    }
+    setUserInputVals(updatedUserInput)
+    setRenderedTitle(newTitle)
   }
 
   const calculateUserInput = val => {
     console.log('calculated user input!', val)
+    console.log('userInputVals in calculateUserInput: ', userInputVals[0], userInputVals[1])
   }
 
-  const handleUserInput = val => {
+  const handleUserInput = val => { // returns f() needed for current title/step of app process
     const handleInput = [userSelectValue, userInputValue, calculateUserInput][
       renderedTitle
     ]
     handleInput(val)
   }
-
   return (
-    <div className='calculator'>
+    <div className='calculator' >
       <Screen
         currentTitle={titles[0][renderedTitle]}
         values={values[0]}
