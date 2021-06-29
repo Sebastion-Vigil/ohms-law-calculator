650eb03b1dbbe77378d86f0f458522443ae0c6e2
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-06-29 01:40:50 -0500

    working functionality with useState, saving cuz it works now, next 2 build Screen component, a child of Calculator useEffect will feed and rerender accordingly

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,11 +4,13 @@ import '../css/calculator.css'
 
 const Calculator = () => {
   const values = useState(['Watts', 'Volts', 'Ohms', 'Amps'])
+  const titles = useState(['Searching for which value?', 'Which 2 values known?', 'Answer'])
   const func = () => {
-    console.log(values[0])
+    console.log(values[0], titles[0])
   }
   return (
     <div className='calculator' onClick={func}>
+        <div className='title'>{titles[0][0]}</div>
         {
             values[0].map((val, i) => {
                 return (
