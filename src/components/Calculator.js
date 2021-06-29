d8998777b6f17702f9e987e1214814af472053bc
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-06-29 01:16:26 -0500

    Screen #1 MVP

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,9 +3,20 @@ import React, { useState } from 'react'
 import '../css/calculator.css'
 
 const Calculator = () => {
-    return (
-        <div className='calculator'>Ohm's Law Calculator</div>
-    )
+  const values = useState(['Watts', 'Volts', 'Ohms', 'Amps'])
+  const func = () => {
+    console.log(values[0])
+  }
+  return (
+    <div className='calculator' onClick={func}>
+        {
+            values[0].map((val, i) => {
+                return (
+                    <div key={i} className='user-select-value'>{val}</div>
+                )
+            })
+        }
+    </div>
+  )
 }
-
 export default Calculator
