dcf94c00079a8423effc47ec0cade2becdc5c3ef
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-07-03 08:58:45 -0500

    test pushing to GitHub

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react' // okay so we got to test this push to GitHub
 
 import Screen from './Screen.js'
 
