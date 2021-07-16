7c337d73409fce31ce18d8cd21691fb10e289ebd
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-07-15 23:07:48 -0500

    fixed title-bar elem repositioning by using flex-start very happy with layout nxt 2 add background imgs add final tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-// we're going to do this the 'right way' this time
-// https://reactjs.org/docs/hooks-overview.html
-// https://www.digitalocean.com/community/tutorials/five-ways-to-convert-react-class-components-to-functional-components-with-react-hooks
-
 import Calculator from './Calculator.js'
 
 import '../css/flexbox.css' // base flex styling
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -11,47 +11,54 @@ const Screen = props => {
   }, [props])
 
   return (
-    <div className='screen flex-col-spc-evn-cntr'>
-      <div className='title-bar flex-col-spc-evn-cntr'>
+    <div className='screen flex-col-flx-strt-cntr'>
+      <div className='title-bar flex-col-cntr-cntr'>
         <div className='title flex-row-cntr-cntr'>{props.currentTitle}</div>
-        <div className='title-display flex-row-flx-end-cntr'>{props.display}</div>
-      </div>
-      {props.values
-        .filter(val => isNaN(parseInt(val)))
-        .map((val, i) => {
-          return (
-            <div
-              key={i}
-              className='user-select-value flex-row-cntr-cntr'
-              onClick={() => {
-                props.handleUser(i)
-              }}
-              style={{
-                visibility: props.bttnVisibility[i]
-              }}
-            >
-              <div className='value-name flex-col-cntr-cntr'>{val}</div>
-              <div
-                className='value-letter flex-col-cntr-cntr'
-              >{'(' + props.letterMap[val] + ')'}</div>
-            </div>
-          )
-        })}
-      {props.keyboardActive ? (
-        <Keyboard
-          handleEnterKey={props.handleEnterKey}
-          handleNumKey={props.handleNumKey}
-          handleDecimalKey={props.handleDecimalKey}
-          handleBackspaceKey={props.handleBackspaceKey}
-          handleClearAllKey={props.handleClearAllKey}
-          handleCancelKey={props.handleCancelKey}
-        />
-      ) : null}
-      {props.resetButton ? (
-        <div className='reset-button flex-col-cntr-cntr' onClick={props.handleResetButton}>
-          Reset
+        <div className='title-display flex-row-flx-end-cntr'>
+          {props.display}
         </div>
-      ) : null}
+      </div>
+      <div className='value-section flex-col-spc-evn-cntr'>
+        {props.values
+          .filter(val => isNaN(parseInt(val)))
+          .map((val, i) => {
+            return (
+              <div
+                key={i}
+                className='user-select-value flex-row-cntr-cntr'
+                onClick={() => {
+                  props.handleUser(i)
+                }}
+                style={{
+                  visibility: props.bttnVisibility[i]
+                }}
+              >
+                <div className='value-name flex-col-cntr-cntr'>{val}</div>
+                <div className='value-letter flex-col-cntr-cntr'>
+                  {'(' + props.letterMap[val] + ')'}
+                </div>
+              </div>
+            )
+          })}
+        {props.keyboardActive ? (
+          <Keyboard
+            handleEnterKey={props.handleEnterKey}
+            handleNumKey={props.handleNumKey}
+            handleDecimalKey={props.handleDecimalKey}
+            handleBackspaceKey={props.handleBackspaceKey}
+            handleClearAllKey={props.handleClearAllKey}
+            handleCancelKey={props.handleCancelKey}
+          />
+        ) : null}
+        {props.resetButton ? (
+          <div
+            className='reset-button flex-col-cntr-cntr'
+            onClick={props.handleResetButton}
+          >
+            Reset
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
